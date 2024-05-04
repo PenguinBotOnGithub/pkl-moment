dd09dc7d0fcd1bc3e0f319cd57f004d26e2a5c9d
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-05-04 10:57:06 +0700

    [EAI] backend/feat: Generate user_role enum, user model, and programmatically run migrations before starting

--- backend/models/src/types.rs
+++ backend/models/src/types.rs
@@ -1,4 +1,6 @@
-#[derive(diesel_derive_enum::DbEnum, Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(diesel_derive_enum::DbEnum, Debug, Clone, Serialize, Deserialize)]
 #[ExistingTypePath = "crate::schema::sql_types::UserRole"]
 pub enum UserRole {
     Admin,
