b76110c78a53b6c4dff342900aa19cb439fbe839
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-05-05 05:41:34 +0700

    [EAI] backend/fix: Restore schema.rs content from commit fb4787fdca27801332d8209ab8f8136aee22a45d

--- backend/models/src/schema.rs
+++ backend/models/src/schema.rs
@@ -1,2 +1,20 @@
 // @generated automatically by Diesel CLI.
 
+pub mod sql_types {
+    #[derive(diesel::query_builder::QueryId, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "user_role"))]
+    pub struct UserRole;
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::UserRole;
+
+    user (id) {
+        id -> Int4,
+        #[max_length = 20]
+        username -> Varchar,
+        password -> Text,
+        role -> UserRole,
+    }
+}
