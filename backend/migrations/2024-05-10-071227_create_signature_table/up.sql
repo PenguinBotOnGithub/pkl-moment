e8dcd57624a3e3d8b8c235a89d4328061bf5ab40
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-05-13 09:39:03 +0700

    [EAI] backend/refactor: Remove image column, use row id for images' name

--- backend/migrations/2024-05-10-071227_create_signature_table/up.sql
+++ backend/migrations/2024-05-10-071227_create_signature_table/up.sql
@@ -3,6 +3,5 @@ CREATE TABLE
     "signature" (
         id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         name TEXT NOT NULL,
-        title TEXT NOT NULL,
-        image TEXT NOT NULL
+        title TEXT NOT NULL
     );
