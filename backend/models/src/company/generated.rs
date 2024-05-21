464447b6b1abed9e27d623624322cb3a18c057f5
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-05-21 07:33:39 +0700

    backend/refactor: [EAI] Add .optional() to read() function

--- backend/models/src/company/generated.rs
+++ backend/models/src/company/generated.rs
@@ -50,10 +50,14 @@ impl Company {
             .await
     }
 
-    pub async fn read(db: &mut Connection, param_id: i32) -> QueryResult<Self> {
+    pub async fn read(db: &mut Connection, param_id: i32) -> QueryResult<Option<Self>> {
         use crate::schema::company::dsl::*;
 
-        company.filter(id.eq(param_id)).first::<Self>(db).await
+        company
+            .filter(id.eq(param_id))
+            .first::<Self>(db)
+            .await
+            .optional()
     }
 
     /// Paginates through the table where page is a 0-based index (i.e. page 0 is the first page)
