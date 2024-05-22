63e1a42ea4479d5f997d1011443e38d394bbcf88
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-05-22 10:47:51 +0700

    backend/refactor: [EAI] Refactor auth routing to its own file

--- backend/api/src/routes.rs
+++ backend/api/src/routes.rs
@@ -5,13 +5,11 @@ use parking_lot::Mutex;
 use warp::{reject::Rejection, Filter, Reply};
 
 use crate::{
-    auth::{
-        login_handler, refresh_token_handler, register_handler, with_auth, with_auth_with_claims,
-    },
+    auth::{auth_routes, with_auth},
     company::{create_company, delete_company, get_companies, read_company, update_company},
     student::{create_student, delete_student, get_students, read_student, update_student},
     wave::{create_wave, delete_wave, get_waves, read_wave, update_wave},
-    with_db, with_json, with_jwt_key,
+    with_db, with_json,
 };
 
 pub fn routes(
@@ -19,42 +17,9 @@ pub fn routes(
     jwt_key: String,
 ) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
     let api = warp::path("api");
-    let auth = api.and(warp::path("auth"));
     let wave = api.and(warp::path("wave"));
     let student = api.and(warp::path("student"));
     let company = api.and(warp::path("company"));
-
-    // Auth
-
-    let login_route = auth
-        .and(warp::path("login"))
-        .and(warp::path::end())
-        .and(warp::post())
-        .and(with_jwt_key(jwt_key.clone()))
-        .and(with_db(db.clone()))
-        .and(with_json())
-        .and_then(login_handler);
-
-    let register_route = auth
-        .and(warp::path("register"))
-        .and(warp::path::end())
-        .and(warp::post())
-        .and(with_auth(true, jwt_key.clone(), db.clone()))
-        .untuple_one()
-        .and(with_json())
-        .and(with_db(db.clone()))
-        .and_then(register_handler);
-
-    let refresh_route = auth
-        .and(warp::path("refresh"))
-        .and(warp::path::end())
-        .and(warp::get())
-        .and(with_auth_with_claims(false, jwt_key.clone(), db.clone()))
-        .and(with_jwt_key(jwt_key.clone()))
-        .and(with_db(db.clone()))
-        .and_then(refresh_token_handler);
-
-    let auth_routes = login_route.or(register_route).or(refresh_route);
 
     // Wave
 
@@ -223,7 +188,7 @@ pub fn routes(
         .and(warp::any())
         .then(|| async { "Hello, World!" });
 
-    root.or(auth_routes)
+    root.or(api.and(auth_routes(jwt_key.clone(), db.clone())))
         .or(waves_routes)
         .or(students_route)
         .or(companies_route)
