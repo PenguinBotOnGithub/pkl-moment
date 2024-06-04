a0fe3dd4bbb5aec65892735773a02e69b64c7b22
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-06-04 12:29:20 +0700

    backend/refactor: [EAI] Implement serde::Serialize manually for UserRole enum so the first letter of serialized role isn't capitalized

--- backend/models/src/types.rs
+++ backend/models/src/types.rs
@@ -1,8 +1,21 @@
-use serde::{Deserialize, Serialize};
+use serde::ser::SerializeStruct;
+use serde::{Deserialize, Serialize, Serializer};
 
-#[derive(diesel_derive_enum::DbEnum, Debug, Clone, Serialize, Deserialize)]
+#[derive(diesel_derive_enum::DbEnum, Debug, Clone, Deserialize)]
 #[ExistingTypePath = "crate::schema::sql_types::UserRole"]
 pub enum UserRole {
     Admin,
     Advisor,
+}
+
+impl Serialize for UserRole {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            UserRole::Admin => serializer.serialize_unit_variant("UserRole", 0, "admin"),
+            UserRole::Advisor => serializer.serialize_unit_variant("UserRole", 0, "advisor"),
+        }
+    }
 }
