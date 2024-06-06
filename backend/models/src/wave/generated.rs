b2b440a7a16b33d5cb13e6549cbef966816c9e33
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-06-07 05:24:08 +0700

    backend/feat: [EAI] Integrate logging with wave create, update, and delete operations

--- backend/models/src/wave/generated.rs
+++ backend/models/src/wave/generated.rs
@@ -1,10 +1,13 @@
 /* This file is generated and managed by dsync */
 
+use crate::log::{CreateLog, Log};
 use crate::schema::*;
+use crate::types::{Operation, TableRef};
 use diesel::QueryResult;
 use diesel::*;
 use diesel_async::RunQueryDsl;
 use serde::{Deserialize, Serialize};
+use tracing::error;
 
 type Connection = diesel_async::AsyncPgConnection;
 
@@ -41,10 +44,30 @@ pub struct PaginationResult<T> {
 }
 
 impl Wave {
-    pub async fn create(db: &mut Connection, item: &CreateWave) -> QueryResult<Self> {
+    pub async fn create(db: &mut Connection, item: &CreateWave, user_id: i32) -> QueryResult<Self> {
         use crate::schema::wave::dsl::*;
 
-        insert_into(wave).values(item).get_result::<Self>(db).await
+        let res = insert_into(wave).values(item).get_result::<Self>(db).await;
+
+        let Ok(_) = res.as_ref() else {
+            return res;
+        };
+
+        if let Err(e) = Log::create(
+            db,
+            &CreateLog {
+                operation_type: Operation::Create,
+                table_affected: TableRef::Wave,
+                user_id,
+                snapshot: None,
+            },
+        )
+        .await
+        {
+            error!("error logging action: {}", e.to_string());
+        }
+
+        res
     }
 
     pub async fn read(db: &mut Connection, param_id: i32) -> QueryResult<Option<Self>> {
@@ -86,21 +109,98 @@ impl Wave {
         db: &mut Connection,
         param_id: i32,
         item: &UpdateWave,
+        user_id: i32,
     ) -> QueryResult<Option<Self>> {
         use crate::schema::wave::dsl::*;
 
-        diesel::update(wave.filter(id.eq(param_id)))
+        let previous = wave
+            .filter(id.eq(param_id))
+            .first::<Self>(db)
+            .await
+            .optional()?;
+        let Some(previous) = previous else {
+            return Ok(None);
+        };
+
+        let res = diesel::update(wave.filter(id.eq(param_id)))
             .set(item)
             .get_result(db)
             .await
-            .optional()
+            .optional();
+
+        let Ok(_) = res else {
+            return res;
+        };
+
+        if let Err(e) = Log::create(
+            db,
+            &CreateLog {
+                operation_type: Operation::Update,
+                table_affected: TableRef::Wave,
+                user_id,
+                snapshot: match serde_json::to_string(&previous) {
+                    Ok(v) => Some(v),
+                    Err(e) => {
+                        error!("error serializing snapshot to json: {}", e.to_string());
+                        Some(format!(
+                            "error serializing snapshot to json: {}",
+                            e.to_string()
+                        ))
+                    }
+                },
+            },
+        )
+        .await
+        {
+            error!("error logging action: {}", e.to_string());
+        }
+
+        res
     }
 
-    pub async fn delete(db: &mut Connection, param_id: i32) -> QueryResult<usize> {
+    pub async fn delete(db: &mut Connection, param_id: i32, user_id: i32) -> QueryResult<usize> {
         use crate::schema::wave::dsl::*;
 
-        diesel::delete(wave.filter(id.eq(param_id)))
-            .execute(db)
+        let previous = wave
+            .filter(id.eq(param_id))
+            .first::<Self>(db)
             .await
+            .optional()?;
+        let Some(previous) = previous else {
+            return Ok(0);
+        };
+
+        let res = diesel::delete(wave.filter(id.eq(param_id)))
+            .execute(db)
+            .await;
+
+        let Ok(_) = res.as_ref() else {
+            return res;
+        };
+
+        if let Err(e) = Log::create(
+            db,
+            &CreateLog {
+                operation_type: Operation::Delete,
+                table_affected: TableRef::Wave,
+                user_id,
+                snapshot: match serde_json::to_string(&previous) {
+                    Ok(v) => Some(v),
+                    Err(e) => {
+                        error!("error serializing snapshot to json: {}", e.to_string());
+                        Some(format!(
+                            "error serializing snapshot to json: {}",
+                            e.to_string()
+                        ))
+                    }
+                },
+            },
+        )
+        .await
+        {
+            error!("error logging action: {}", e.to_string());
+        }
+
+        res
     }
 }
