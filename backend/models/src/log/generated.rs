b0aa1797bd31b397b446f876e56dbe9c87ea12e8
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-06-06 23:51:04 +0700

    backend/refactor: [EAI] Use .optional() when doing reading operations filtered by id

--- backend/models/src/log/generated.rs
+++ backend/models/src/log/generated.rs
@@ -68,10 +68,13 @@ impl Log {
         insert_into(log).values(item).get_result::<Self>(db).await
     }
 
-    pub async fn read(db: &mut Connection, param_id: i32) -> QueryResult<Self> {
+    pub async fn read(db: &mut Connection, param_id: i32) -> QueryResult<Option<Self>> {
         use crate::schema::log::dsl::*;
 
-        log.filter(id.eq(param_id)).first::<Self>(db).await
+        log.filter(id.eq(param_id))
+            .first::<Self>(db)
+            .await
+            .optional()
     }
 
     /// Paginates through the table where page is a 0-based index (i.e. page 0 is the first page)
