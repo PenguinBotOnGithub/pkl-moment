1c5f9443e8196894012b50301f96fe9657b30d81
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-06-08 16:00:28 +0700

    backend/fix: [EAI] Handle possible fk error when adding students to letters

--- backend/api/src/permohonan.rs
+++ backend/api/src/permohonan.rs
@@ -415,7 +415,7 @@ async fn add_permohonan_student(
                     claims.id,
                 )
                 .await
-                .map_err(|e| reject::custom(InternalError::DatabaseError(e.to_string())))?;
+                .map_err(handle_fk_data_not_exists)?;
 
                 Ok(reply::json(&ApiResponse::ok("success".to_owned(), res)))
             }
@@ -435,7 +435,7 @@ async fn add_permohonan_student(
                     claims.id,
                 )
                 .await
-                .map_err(|e| reject::custom(InternalError::DatabaseError(e.to_string())))?;
+                .map_err(handle_fk_data_not_exists)?;
 
                 Ok(reply::json(&ApiResponse::ok("success".to_owned(), res)))
             }
