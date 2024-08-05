34f2d14e744fc47cc1263520c42c07c42b0185fb
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-08-05 09:26:23 +0700

    backend/fix: [EAI] Remove unused import

--- backend/api/src/journal.rs
+++ backend/api/src/journal.rs
@@ -10,7 +10,6 @@ use models::tenure::Tenure;
 use models::types::UserRole;
 use parking_lot::Mutex;
 use std::{collections::HashMap, num::ParseIntError, sync::Arc};
-use tracing::error;
 use warp::{
     reject::{self, Rejection},
     reply::{self, Reply},
