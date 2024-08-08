eec839f92689896d3c502d65c9b65e7aa87006c5
PenguinBotOnGithub <penguinbotonline@protonmail.com>
2024-08-08 11:49:12 +0700

    backend/feat: [EAI] Add upload company's mou route and serve mou dir

--- backend/api/src/company.rs
+++ backend/api/src/company.rs
@@ -1,20 +1,29 @@
 use std::{collections::HashMap, num::ParseIntError, sync::Arc};
 
 use diesel_async::AsyncPgConnection;
-use models::company::{Company, CreateCompany, UpdateCompany};
+use models::{
+    company::{Company, CreateCompany, UpdateCompany},
+    log::Log,
+    types::{Operation, TableRef},
+};
 use parking_lot::Mutex;
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
+use tokio::{fs, io::AsyncWriteExt};
 use warp::{
     reject::{self, Rejection},
     reply::{self, Reply},
-    Filter,
+    Buf, Filter,
 };
 
-use crate::auth::{with_auth_with_claims, JwtClaims};
 use crate::error::handle_fk_depended_data_delete;
 use crate::{
     auth::with_auth,
     error::{ClientError, InternalError},
     with_db, with_json, ApiResponse,
+};
+use crate::{
+    auth::{with_auth_with_claims, JwtClaims},
+    with_doc_upload,
 };
 
 pub fn companies_routes(
@@ -68,11 +77,22 @@ pub fn companies_routes(
         .and(with_db(db.clone()))
         .and_then(delete_company);
 
+    let upload_mou_route = company
+        .and(warp::path::param::<i32>())
+        .and(warp::path("mou"))
+        .and(warp::path::end())
+        .and(warp::post())
+        .and(with_auth_with_claims(true, jwt_key.clone(), db.clone()))
+        .and(with_doc_upload())
+        .and(with_db(db.clone()))
+        .and_then(upload_doc);
+
     get_companies_route
         .or(create_company_route)
         .or(read_company_route)
         .or(update_company_route)
         .or(delete_company_route)
+        .or(upload_mou_route)
 }
 
 async fn get_companies(
@@ -174,4 +194,91 @@ async fn delete_company(
             "company not found".to_owned(),
         )))
     }
+}
+
+async fn upload_doc(
+    id: i32,
+    claims: JwtClaims,
+    filetype: &'static str,
+    mut body: impl Buf,
+    db: Arc<Mutex<AsyncPgConnection>>,
+) -> Result<impl Reply, Rejection> {
+    {
+        let mut db = db.lock();
+        let exists = Company::check_existence(&mut db, id)
+            .await
+            .map_err(|e| InternalError::DatabaseError(e.to_string()))?;
+        if let None = exists {
+            return Err(reject::custom(ClientError::NotFound(
+                "company not found".to_owned(),
+            )));
+        }
+    }
+
+    let mut doc: Vec<u8> = Vec::with_capacity(1024 * 5000);
+    while body.has_remaining() {
+        let chunk = body.chunk();
+        doc.extend_from_slice(chunk);
+        let count = chunk.len();
+        body.advance(count);
+    }
+
+    let mut count = 0;
+    let mut file = loop {
+        let filename: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(15)
+            .map(char::from)
+            .collect();
+
+        let f = fs::File::create_new(format!("assets/mou/{filename}.{filetype}")).await;
+        match f {
+            Ok(f) => break Ok((f, filename)),
+            Err(e) => {
+                count += 1;
+                if count > 5 {
+                    break Err(reject::custom(InternalError::FilesystemError(
+                        e.to_string(),
+                    )));
+                } else {
+                    continue;
+                }
+            }
+        }
+    }?;
+
+    file.0
+        .write_all(&doc)
+        .await
+        .map_err(|e| InternalError::FilesystemError(e.to_string()))?;
+
+    let mut db = db.lock();
+    Log::log(
+        &mut db,
+        Operation::Upload,
+        TableRef::Company,
+        *&claims.id,
+        None::<u8>,
+    )
+    .await;
+
+    let u = Company::update(
+        &mut db,
+        id,
+        &UpdateCompany {
+            name: None,
+            address: None,
+            mou_url: Some(Some(format!("/assets/mou/{}.{}", file.1, filetype))),
+        },
+        *&claims.id,
+    )
+    .await
+    .map_err(|e| InternalError::DatabaseError(e.to_string()))?;
+    let Some(u) = u else {
+        return Err(reject::custom(ClientError::NotFound(
+            "company not found".to_owned(),
+        )));
+    };
+
+    Ok(reply::json(&ApiResponse::ok("success".to_owned(), u)))
 }
